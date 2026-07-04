a58284563ed7035ff446fff9a33226829e4ba8b7
cheefds <chengzhewu0@gmail.com>
2026-07-04 15:36:37 +0800

    修改show_rtree，新增show_rtree_mbr

--- rtree_show.py
+++ rtree_show.py
@@ -4,11 +4,27 @@ def show_rtree(root,level = 0):
     print("Level", level, ":", end=" ")
     for entry in root.children:
         if entry.is_leaf_entry():
-            print(entry.mbr,entry.data, end=" ")
+            print(entry.data, end=" ")
             
         else:
-            print(entry.mbr, f"Node({len(entry.child.children)})", end=" ")
+            print(f"Node({len(entry.child.children)})", end=" ")
     print()
     for entry in root.children:
         if entry.child is not None:
             show_rtree(entry.child, level + 1)
+    
+    
+def show_rtree_mbr(root,level = 0):
+    if root is None:
+        return
+    print("Level", level, ":", end=" ")
+    for entry in root.children:
+        if entry.is_leaf_entry():
+            print(f"MBR:{entry.mbr},Data", end=" ")
+        else:
+            print(f"MBR:{entry.mbr},Link", end=" ")
+    print()
+    for entry in root.children:
+        if entry.child is not None:
+            show_rtree_mbr(entry.child, level + 1)
+    
